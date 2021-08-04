d89ac4e7c420a98761d99473b209ae0155ee05cc
CemAdg <cem.akdag@hotmail.de>
2021-08-04 18:57:36 +0200

    app.py updated: APIs for different device types added

--- backend/app.py
+++ backend/app.py
@@ -35,23 +35,176 @@ def create_app(test_config=None):
 
     """
     GET /api
-    Get all registered store devices
+    Get all registered store device types
     """    
     @app.route('/api', methods=['GET'])
-    def get_store_devices():
+    def get_devices():
         
         try:
    
-            store_devices = {"Pfandautomat","Waage"}
+            devices = {
+                1: "Pfandautomat",
+                2: "Waage"
+            }
         
             return jsonify({
                 "success": True,
-                "store_devices": store_devices
+                "device": devices
             })
 
 
         except BaseException:
             abort(422)
+
+    """
+    GET /api/pfandautomaten
+    Get all available devices of type Pfandautomat in the store
+    """    
+    @app.route('/api/pfandautomaten', methods=['GET'])
+    def get_pfandautomaten():
+        
+        try:
+   
+            pfandautomaten = {
+                1: "P001",
+                2: "P002",
+                3: "P003"
+            }
+        
+            return jsonify({
+                "success": True,
+                "pfandautomaten": pfandautomaten
+            })
+
+
+        except BaseException:
+            abort(422)
+
+
+    """
+    GET /api/pfandautomaten/{id}
+    Get all available services of specfific Pfandautomat with id
+    """    
+    @app.route('/api/pfandautomaten/<int:pfandautomat_id>', methods=['GET'])
+    def get_pfandautomaten_services(pfandautomat_id):
+        
+        try:
+   
+            pfandautomat_services = {
+                1: "Füllstand",
+                2: "Störungen",
+                3: "Wartungsaktivitäten"
+            }
+        
+            return jsonify({
+                "success": True,
+                "pfandautomat_id": pfandautomat_id,
+                "pfandautomat_services": pfandautomat_services
+            })
+
+
+        except BaseException:
+            abort(422)
+
+
+    """
+    GET /api/pfandautomaten/{id}/füllstand
+    Get füllstand of Pfandautomat with id
+    """    
+    @app.route('/api/pfandautomaten/<int:pfandautomat_id>/füllstand', methods=['GET'])
+    def get_pfandautomaten_services_füllstand(pfandautomat_id):
+        
+        try:
+   
+            füllstand = "50%"
+        
+            return jsonify({
+                "success": True,
+                "pfandautomat_id": pfandautomat_id,
+                "füllstand": füllstand
+            })
+
+
+        except BaseException:
+            abort(422)
+
+
+
+    """
+    GET /api/waagen
+    Get Get all available devices of type Waage in the store
+    """    
+    @app.route('/api/waagen', methods=['GET'])
+    def get_waagen():
+        
+        try:
+
+            waagen = {
+                1: "W001",
+                2: "W002"
+            }
+        
+            return jsonify({
+                "success": True,
+                "waagen": waagen
+            })
+
+
+        except BaseException:
+            abort(422)
+
+
+    """
+    GET /api/waagen/{id}
+    Get all available services of specfific Waage with id
+    """    
+    @app.route('/api/waagen/<int:waage_id>', methods=['GET'])
+    def get_waagen_services(waage_id):
+        
+        try:
+   
+            waage_services = {
+                1: "Füllstand",
+                2: "Störungen",
+                3: "Wartungsaktivitäten"
+            }
+        
+            return jsonify({
+                "success": True,
+                "waage_services": waage_services
+            })
+
+
+        except BaseException:
+            abort(422)
+
+        
+    """
+    GET /api/waagen/{id}/störungen
+    Get Störungen of Waage with id
+    """    
+    @app.route('/api/waagen/<int:waage_id>/störungen', methods=['GET'])
+    def get_waagen_services_störungen(waage_id):
+        
+        try:
+   
+            störungen = { 
+                "Komponente":"Drucker",
+                "Beschreibung":"Drucker ist defekt."
+            }
+        
+            return jsonify({
+                "success": True,
+                "waage_id": waage_id,
+                "störungen": störungen
+            })
+
+
+        except BaseException:
+            abort(422)
+
+
+
 
 
     '''
