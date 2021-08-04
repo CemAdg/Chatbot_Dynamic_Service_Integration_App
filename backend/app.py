1029535a097ed7780c0365a40217e590acacb840
CemAdg <cem.akdag@hotmail.de>
2021-08-04 18:22:50 +0200

    app.py updated

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,6 @@
-#first app
 import os
 import json
 import time
-import RPi.GPIO as GPIO
 
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
@@ -36,70 +34,19 @@ def create_app(test_config=None):
 
 
     """
-    GET /lamp/<int:lamp_id>/activate
-    Activate lamp according to the lamp_id that is sent from the corresponding button in the React App Client
+    GET /api
+    Get all registered store devices
     """    
-    @app.route('/lamp/<int:lamp_id>/activate', methods=['GET'])
-    def activate_lamp(lamp_id):
+    @app.route('/api', methods=['GET'])
+    def get_store_devices():
         
-        if lamp_id > 3:
-            abort(404)
-
         try:
-            GPIO.setmode(GPIO.BOARD)
+   
+            store_devices = {"Pfandautomat","Waage"}
         
-            if lamp_id == 1:
-                # Leuchte 1 für Reaktion "Frage" (BLAU)
-                
-                # set GPIO pins, requesting pins bei the board numbers (1-40)
-                #GPIO.setmode(GPIO.BOARD)
-                GPIO.setup(11,GPIO.OUT)
-                GPIO.output(11,GPIO.LOW)
-
-                # activate GPIO pins
-                GPIO.output(11,GPIO.HIGH)
-                time.sleep(5)
-
-                # deactivate GPIO pins
-                GPIO.output(11,GPIO.LOW)
-               
-
-            if lamp_id == 2:
-                # Leuchte 2 für Reaktion "Daumen hoch" (GRÜN)
-
-                # set GPIO pins, requesting pins bei the board numbers (1-40)
-                #GPIO.setmode(GPIO.BOARD)
-                GPIO.setup(13,GPIO.OUT)
-                GPIO.output(13,GPIO.LOW)
-
-                # activate GPIO pins
-                GPIO.output(13,GPIO.HIGH)
-                time.sleep(5)
-
-                # deactivate GPIO pins
-                GPIO.output(13,GPIO.LOW)
-                
-
-            if lamp_id == 3:
-                # Leuchte 3 für Reaktion "Problem" (ROT)
-
-                # set GPIO pins, requesting pins bei the board numbers (1-40)
-                #GPIO.setmode(GPIO.BOARD)
-                GPIO.setup(15,GPIO.OUT)
-                GPIO.output(15,GPIO.LOW)
-
-                # activate GPIO pins
-                GPIO.output(15,GPIO.HIGH)
-                time.sleep(5)
-
-                # deactivate GPIO pins
-                GPIO.output(15,GPIO.LOW)
-                
-            GPIO.cleanup()
-
             return jsonify({
-                'lamp_id': lamp_id,
-                'success': True
+                "success": True,
+                "store_devices": store_devices
             })
 
 
